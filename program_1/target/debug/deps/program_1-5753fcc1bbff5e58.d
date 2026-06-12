2a8fc2db1d189699020859ef75376790746aaaec
jeanyvesruffin <ruffinmax@hotmail.fr>
2026-06-12 16:23:35 +0200

    fin TD

---
+++ program_1/target/debug/deps/program_1-5753fcc1bbff5e58.d
@@ -0,0 +1,5 @@
+C:\Users\ruffi\WORKSPACE\rust\program_1\target\debug\deps\program_1-5753fcc1bbff5e58.d: src\main.rs
+
+C:\Users\ruffi\WORKSPACE\rust\program_1\target\debug\deps\libprogram_1-5753fcc1bbff5e58.rmeta: src\main.rs
+
+src\main.rs:
